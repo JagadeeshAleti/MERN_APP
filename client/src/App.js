f4c9db17f2f934ce4bd503f23f579f7ca1b091bc
jagadeeshaleti <jagadeswara@ecyber.com>
2022-08-23 17:37:42 +0530

    Add a basic routing

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Register/Register.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const register = () => {
+  return <div className="register">return register</div>;
+};
+
+export default register;
