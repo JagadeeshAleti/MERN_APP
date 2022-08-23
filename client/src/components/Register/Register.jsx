b59957c0b9b44dd81f10a86eb83b9c21f3b32258
jagadeeshaleti <jagadeswara@ecyber.com>
2022-08-23 18:24:03 +0530

    Design register Page

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,7 +1,88 @@
 import React from "react";
+import { Box, Grid, Typography } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { borderLeft } from "@mui/system";
 
 const register = () => {
-  return <div className="register">return register</div>;
+  const [email, setEmail] = useState();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+
+  const onSubmitHandler = () => {
+    console.log(email, password);
+  };
+
+  return (
+    <Grid container item xs={12} sm={4} m={"auto"} rowGap={2} mt={"10%"}>
+      <Grid item xs={12}>
+        <Typography
+          sx={{
+            color: "blue",
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: 24,
+          }}
+        >
+          Register
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          color="primary"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          color="primary"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          id="outlined-basic"
+          label="Username"
+          variant="outlined"
+        ></TextField>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          id="filled-basic"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          variant="outlined"
+        />
+      </Grid>
+      <Grid xs={12}>
+        <TextField
+          fullWidth
+          id="filled-basic"
+          label="Confirm Password"
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => {
+            setConfirmPassword(e.target.value);
+          }}
+          variant="outlined"
+        ></TextField>
+      </Grid>
+      <Grid item xs={12}>
+        <Button fullWidth onClick={onSubmitHandler} variant="contained">
+          Contained
+        </Button>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default register;
