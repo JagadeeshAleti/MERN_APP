f22d8af2b1ff39eb2f5aaadf790c5018e7a56f14
jagadeeshaleti <jagadeswara@ecyber.com>
2022-09-26 09:52:24 +0530

    Refactoring refresh token

--- vendor-client/src/http/http.js
+++ vendor-client/src/http/http.js
@@ -4,54 +4,54 @@ import { getConfig } from "../config";
 
 const url = getConfig().backend;
 
+const withRefreshToken = async (fn) => {
+  try {
+    return await fn();
+  } catch (e) {
+    if (_.get(e, "response.data.code") === "TOKEN_EXPIRED") {
+      const refreshToken = localStorage.getItem("refreshToken");
+      const res = await axios.post(
+        `${url}/user/refreshToken`,
+        {},
+        {
+          headers: { Authorization: refreshToken },
+        }
+      );
+      localStorage.setItem("token", _.get(res, "data.newToken"));
+      localStorage.setItem("refreshToken", _.get(res, "data.newRefreshToken"));
+    }
+    return await fn();
+  }
+};
+
 export const HttpClient = {
   post: async (subUrl, body) => {
-    try {
+    return withRefreshToken(async () => {
       const res = await axios.post(`${url}/${subUrl}`, body);
       return res;
-    } catch (e) {
-      console.log("error", e);
-    }
+    });
   },
 
   getAll: () => {},
 
   get: async (subUrl) => {
-    try {
+    return withRefreshToken(async () => {
       const token = localStorage.getItem("token");
       const result = await axios.get(`${url}/${subUrl}`, {
         headers: { Authorization: token },
       });
       return result.data;
-    } catch (e) {
-      if (_.get(e, "response.data.code") === "TOKEN_EXPIRED") {
-        const refreshToken = localStorage.getItem("refreshToken");
-        const res = await axios.post(
-          `${url}/user/refreshToken`,
-          {},
-          {
-            headers: { Authorization: refreshToken },
-          }
-        );
-        localStorage.setItem("token", _.get(res, "data.newToken"));
-        localStorage.setItem(
-          "refreshToken",
-          _.get(res, "data.newRefreshToken")
-        );
-      }
-    }
+    });
   },
 
   put: async (subUrl, body) => {
-    try {
+    return withRefreshToken(async () => {
       const token = localStorage.getItem("token");
       const result = await axios.put(`${url}/${subUrl}`, body, {
         headers: { Authorization: token },
       });
       return result;
-    } catch (e) {
-      console.log("error", e);
-    }
+    });
   },
 
   delete: (id) => {},
